6e99a23432ab4f6163225679c43cb2c2650e7f58
unknown <naim.ahmed035@gmail.com>
2021-07-31 19:20:14 +0600

    Add UserModule to AppModule & Find user on UserService

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,16 @@ import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      `mongodb://localhost:27017/nestjs-phonebook`
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './users.model';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(@InjectModel("User") private readonly userModel: Model<User>) {};
+
+    /**
+     * Get all Users from Database
+     * 
+     * @returns 
+     */
+    async getAllUsers() {
+        const users = await this.userModel.find().exec();
+
+        return users.map((user) => ({
+            id: user.id,
+            name: user.name,
+            cell: user.cell,
+            age: user.age
+        }));
+    }
+
+    /**
+     * Get one user from Database
+     * 
+     * @param userId 
+     * @returns 
+     */
+    async getOneUser(userId: string) {
+        const user = await this.findUser(userId);
+
+        return {
+            id: user.id,
+            name: user.name,
+            cell: user.cell,
+            age: user.age
+        };
+    }
+
+    /**
+     * 
+     * @param id 
+     * @returns 
+     */
+    private async findUser(id: string): Promise<User> {
+        let user: any;
+
+        try {
+            user = await this.userModel.findById(id).exec();
+        } catch(error) {
+            throw new NotFoundException("Sorry! Unable to find the user");
+        }
+
+        if(!user) {
+            throw new NotFoundException("Sorry! Unable to find the user");
+        }
+
+        return user;
+    }
+}
