aad6c10e30b87878b14f61c37a2cd7c9f727661c
unknown <naim.ahmed035@gmail.com>
2021-07-31 19:24:19 +0600

    Find User on Controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly userService: UsersService) {}
+
+    @Get()
+    getAllUsers() {
+        return this.userService.getAllUsers();
+    }
+
+    @Get(":id")
+    getOneUser(@Param("id") userId: string) {
+        return this.userService.getOneUser(userId);
+    }
+}
